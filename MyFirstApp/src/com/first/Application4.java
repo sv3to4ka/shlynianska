7758ff82a21c49817a9bd0f8e234e9a5fc777869
shlynianska <shlynyanska@gmail.com>
2020-05-08 18:16:49 +0300

    basic: ArrayList, LinkedList

--- MyFirstApp/src/com/first/Application4.java
+++ MyFirstApp/src/com/first/Application4.java
@@ -1,11 +1,40 @@
 package com.first;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 public class Application4 {
     public static void main(String[] args) {
-        Application3.main(new String[0]);
-        Application3.name = "456";
+        List<String> fruit = new ArrayList<String>();
+        fruit.add("apple");
+        fruit.add("peach");
+        fruit.add("pear");
+        System.out.println(fruit);
 
+        List<String> vegetables = new LinkedList<String>();
+        vegetables.add("beetroot");
+        vegetables.add("carrot");
+        vegetables.add("cabbage");
+        System.out.println(vegetables);
 
+        fruit.remove(0);
+        System.out.println(fruit);
+
+        vegetables.remove(2);
+        System.out.println(vegetables);
+
+        fruit.set(1, "yellow yellow yellow yellow");
+        System.out.println(fruit);
+
+        fruit.set(0, "first");
+        fruit.set(1, "second");
+        System.out.println(fruit);
+
+        vegetables.set(0, "first");
+        vegetables.set(1, "second");
+        System.out.println(vegetables);
+
+        System.out.println(fruit.equals(vegetables));
 
     }
 }
