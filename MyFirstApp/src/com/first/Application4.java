82c137876baaafc66930ea615152d8f9c2440b0f
shlynianska <shlynyanska@gmail.com>
2020-05-11 17:33:31 +0300

    basic: HashMap, TreeMap, HashSet, TreeSet, LinkedHashSet

--- MyFirstApp/src/com/first/Application4.java
+++ MyFirstApp/src/com/first/Application4.java
@@ -1,8 +1,7 @@
 package com.first;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+
 public class Application4 {
     public static void main(String[] args) {
         List<String> fruit = new ArrayList<String>();
@@ -35,6 +34,78 @@ public class Application4 {
         System.out.println(vegetables);
 
         System.out.println(fruit.equals(vegetables));
+        //HashMap
+        HashMap<String, String> body = new HashMap<String, String>();
+        body.put("Hair", "Hairstyle");
+        body.put("Eyebrows", "Tattoo");
+        body.put("Eyes", "Makeup");
+        body.put("Nose", "Runny nose");
+        body.put("Lips", "Lipstick");
+        body.put("Teeth", "Smile");
+        body.put("Neck", "Necklace");
+        body.put("111", "222");
+        System.out.println(body.size());
+        body.remove("111");
+        System.out.println(body);
+        System.out.println(body.size());
+        //print keys and values
+        for (String i: body.keySet()) {
+            System.out.println(i);
+        }
+        for (String a: body.values()) {
+            System.out.println(a);
+        }
+        //TreeMap
+        TreeMap<String, String> bodyParts = new TreeMap<String, String>();
+        bodyParts.put("Head", "Ear");
+        bodyParts.put("Hand", "Finger");
+        bodyParts.put("Shoulder", "Stomach");
+        bodyParts.put("Leg", "Foot");
+        System.out.println(bodyParts);
+        System.out.println(bodyParts.size());
+        for (String i: bodyParts.keySet()) {
+            System.out.println(i);
+            }
+        for (String b: bodyParts.values()) {
+            System.out.println(b);
+        }
+        //HashSet
+        HashSet<String> doing1 = new HashSet<String>();
+        doing1.add("drink");
+        doing1.add("eat");
+        doing1.add("smell");
+        doing1.add("move");
+        doing1.add(null);
+        doing1.add("move");
+        System.out.println(doing1);
+        System.out.println(doing1.size());
+        //TreeSet
+        TreeSet<String> doing2 = new TreeSet<String>();
+        doing2.add("run");
+        doing2.add("fly");
+        doing2.add("crawl");
+        doing2.add("walk");
+        doing2.add("null");
+        doing2.add("run");
+        System.out.println(doing2);
+        System.out.println(doing2.size());
+        //LinkedHashSet
+        LinkedHashSet<String> doing3 = new LinkedHashSet<String>();
+        doing3.add("look at");
+        doing3.add("look for");
+        doing3.add("look after");
+        doing3.add("look around");
+        doing3.add("look into");
+        doing3.add("look on");
+        doing3.add("look out");
+        doing3.add("look up");
+        doing3.add("look up");
+        doing3.add(null);
+        System.out.println(doing3);
+        System.out.println(doing3.size());
 
+        for (Map.Entry<String, String> entry : body.entrySet()) {
+            System.out.println("key: " + entry.getKey() + ", value: " + entry.getValue());
+        }
     }
 }
