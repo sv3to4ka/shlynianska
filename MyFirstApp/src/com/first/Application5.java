9efcf47c8fe748e5eee53fb87c5d3135a36bc9ca
shlynianska <shlynyanska@gmail.com>
2020-05-17 13:56:22 +0300

    basic: try-catch_ArrayList

--- MyFirstApp/src/com/first/Application5.java
+++ MyFirstApp/src/com/first/Application5.java
@@ -1,5 +1,8 @@
 package com.first;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application5 {
     public static void main(String[] args) {
         //catch Exception; openHierarchy click + ctrl+h
@@ -9,6 +12,19 @@ public class Application5 {
         } catch (Exception id) {
             System.out.println(id.getMessage());
         }
+        List<String> fingers = new ArrayList<String>();
+        fingers.add("first");
+        fingers.add("second");
+        System.out.println(fingers);
+        try {
+            if (fingers.size() < 3) {
+                throw new IllegalArgumentException("invalid data");
+            }
+        } catch (Exception notFound) {
+            System.out.println(notFound.getMessage());
 
+        }
     }
 }
+
+
